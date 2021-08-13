bd3b224084be52285984a9c61df1151e263e7091
avadhoot <avadhootautade111@gmail.com>
2021-08-13 19:26:16 +0530

    requirements updated

--- Craigslist/settings.py
+++ Craigslist/settings.py
@@ -83,11 +83,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ciba',
-<<<<<<< HEAD
-
-=======
-        
->>>>>>> 4ce8389b6d6dbca9cd2d6e41c10b0d1b17e2d15d
     }
 }
 db_from_env = dj_database_url.config(conn_max_age=500)
