886696b622823f91cd1e5f063f39c36285b3a30f
avadhoot <avadhootautade111@gmail.com>
2021-08-13 10:11:54 +0530

    next commit

---
+++ craigs/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CraigsConfig(AppConfig):
+    name = 'craigs'
---
+++ craigs/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'base.html')
+
+
+def new_search(request):
+    return render(request, 'craigs/new_search.html')
